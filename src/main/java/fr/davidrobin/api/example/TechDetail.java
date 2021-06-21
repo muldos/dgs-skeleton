393f6f81198cc37f6eab715a0e26dd3c13766651
David ROBIN <david.a.robin@gmail.com>
2021-06-21 17:16:05 +0200

    add hostname query

---
+++ src/main/java/fr/davidrobin/api/example/TechDetail.java
@@ -0,0 +1,18 @@
+package fr.davidrobin.api.example;
+
+public class TechDetail {
+    private String hostname;
+
+    public TechDetail() {
+        super();
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+    
+}
---
+++ src/main/java/fr/davidrobin/api/example/TechDetailDatafetecher.java
@@ -0,0 +1,21 @@
+package fr.davidrobin.api.example;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+
+@DgsComponent
+public class TechDetailDatafetecher {
+    @DgsQuery
+    public TechDetail diagnosis() {
+        TechDetail t = new TechDetail();
+        try {
+            t.setHostname(InetAddress.getLocalHost().getHostName());
+        }
+        catch(UnknownHostException e) {
+            t.setHostname("unknown with error");
+        }
+        return t;
+    }
+}
