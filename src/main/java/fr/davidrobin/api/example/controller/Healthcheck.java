869cbebe5b02dd0b7568ce1ffd3f832cccf8dd75
David ROBIN <david.a.robin@gmail.com>
2021-06-27 15:16:04 +0200

    add health check dynamic configuration

--- src/main/java/fr/davidrobin/api/example/controller/Healthcheck.java
+++ src/main/java/fr/davidrobin/api/example/controller/Healthcheck.java
@@ -1,14 +1,34 @@
 package fr.davidrobin.api.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/health")
 public class Healthcheck {
+	private Boolean status = Boolean.TRUE;
     @ResponseBody
-    @GetMapping("/health")
-	public String healthy() {
-		return "health : ok";
+    @GetMapping("/status")
+	public ResponseEntity<String> healthy() {
+		ResponseEntity<String> response = (this.status) ? new ResponseEntity<String>("ok",HttpStatus.OK) : new ResponseEntity<String>("ko",HttpStatus.SERVICE_UNAVAILABLE);
+		return response;
+	}
+
+	@ResponseBody
+    @GetMapping("/off")
+	public String setOff() {
+		this.status = Boolean.FALSE;
+		return "health status was set off";
+	}
+
+	@ResponseBody
+    @GetMapping("/on")
+	public String setOn() {
+		this.status = Boolean.TRUE;
+		return "health status was set on";
 	}
 }
