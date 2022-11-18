80f9e57248018e5019e5a6200f8c0e9fa48ae584
muldos <davidro@jfrog.com>
2022-11-18 15:48:08 +0100

    remove the vulnerable code

--- src/main/java/fr/davidrobin/api/controller/UnsafeController.java
+++ src/main/java/fr/davidrobin/api/controller/UnsafeController.java
@@ -1,10 +1,8 @@
 package fr.davidrobin.api.controller;
 
-import fr.davidrobin.api.bean.EvalBean;
-
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,11 +14,10 @@ public class UnsafeController {
         String[] blackList = { "class.*", "Class.*", "*.class.*", ".*Class.*" };
         binder.setDisallowedFields(blackList);
     }
-    // Vulnerable code for CVE-2022-22965
-    // https://jfrog.com/blog/springshell-zero-day-vulnerability-all-you-need-to-know/
-    @RequestMapping("/vulnerable-path")
-    public String VulnerablePath(EvalBean evalBean) {
 
-        return "Hello, I'm a vulnerability : " + evalBean;
+    @GetMapping("/vulnerable-path")
+    public String VulnerablePath() {
+
+        return "Hello, I'm a vulnerability : " ;
     }
 }
