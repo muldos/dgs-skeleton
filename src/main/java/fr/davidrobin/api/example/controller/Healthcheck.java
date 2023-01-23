ed9906307eb8c59a0046fc3926ac9b7558491a74
guybar <guy.yuval.baharav@gmail.com>
2023-01-23 17:42:26 +0200

    Update Healthcheck.java

--- src/main/java/fr/davidrobin/api/example/controller/Healthcheck.java
+++ src/main/java/fr/davidrobin/api/example/controller/Healthcheck.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/healt")
+@RequestMapping("/health")
 public class Healthcheck {
 	private Boolean status = Boolean.TRUE;
 
