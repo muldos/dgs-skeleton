e5e15537e56e26430dab3bd415e08a4574573f2c
muldos <davidro@jfrog.com>
2023-02-27 11:13:47 +0100

    hotfix of springshell

--- src/main/java/fr/davidrobin/api/controller/UnsafeController.java
+++ src/main/java/fr/davidrobin/api/controller/UnsafeController.java
@@ -1,12 +1,11 @@
 package fr.davidrobin.api.controller;
-import fr.davidrobin.api.bean.EvalBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UnsafeController {
     @RequestMapping("/vulnerable-path")
-    public String VulnerablePath(EvalBean evalBean){
-        return "Hello, I'm a vulnerability : " + evalBean;
+    public String VulnerablePath(){
+        return "Hello, I'm not a vulnerability " ;
     }
 }
