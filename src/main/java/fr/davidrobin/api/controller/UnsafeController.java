d44db89b1f9409c810b9b58a6b12f5f9bb186bf1
David Robin <davidro@jfrog.com>
2023-04-21 17:21:46 +0200

    display maven version

--- src/main/java/fr/davidrobin/api/controller/UnsafeController.java
+++ src/main/java/fr/davidrobin/api/controller/UnsafeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UnsafeController {
+    public static final String login = "AKIAJXBOVX5Q2EULDUIA";
+    public static final String pwd ="SqcyDpetv+pCsbNYWHDLE8yR5mJ13MI+4d8NOwtM";
     @RequestMapping("/vulnerable-path")
     public String VulnerablePath(EvalBean evalBean){
         return "Hello, I'm a vulnerability : " + evalBean;
